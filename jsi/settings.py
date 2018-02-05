b4689518543f6cf5653939f2448517ab6937c9da
Park Hyunwoo <ez.amiryo@gmail.com>
2018-02-05 19:28:17 +0900

    Apply django_heroku

--- jsi/settings.py
+++ jsi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y&29zb&x4crptfyz!fqrxl=y*ba%s8evb5$b9*@#-19yc%qvvf'
+SECRET_KEY = None
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
